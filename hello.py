1f5dc79f7df696defbebd4e949bafe32ce28d19d
Nguyễn Dương <nguyenduongit89@gmail.com>
2025-09-01 19:13:03 +0700

    Update label padding in hello.py for improved layout

--- hello.py
+++ hello.py
@@ -4,5 +4,5 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Hello World")
 label = tk.Label(root, text="Hello World!!", font=("Arial", 20))
-label.pack(padx=50, pady=50)
+label.pack(padx=1200, pady=800)
 root.mainloop()
