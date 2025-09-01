aa408f10f185b407e150b62a92a7112aa24202a1
Nguyễn Dương <nguyenduongit89@gmail.com>
2025-09-01 18:18:54 +0700

    test

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 import tkinter as tk
 
 root = tk.Tk()
-root.title("Hello World")
+root.title("Hello World 2")
 label = tk.Label(root, text="Hello World!", font=("Arial", 20))
 label.pack(padx=50, pady=50)
 root.mainloop()
